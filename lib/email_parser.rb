882c2c1ba252bed6575bb9aa5b5023bd40460a3c
Dana Palmer <roiai4@hotmail.com>
2018-10-19 02:58:46 +0000

    automatically backed up by learn

--- lib/email_parser.rb
+++ lib/email_parser.rb
@@ -13,17 +13,3 @@ class EmailParser
   end
 
 end
-
-class EmailParser
-  attr_reader :emails
-
-  def initialize(emails)
-    @emails = emails
-  end
-
-  def parse
-    emails.split.map do |email|
-      email.split(',')
-    end.flatten.uniq
-  end
-end
